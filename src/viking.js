6331a3334d39c1403fe5714aad958665d837fcf1
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-06-06 23:14:08 -0300

    lab finalizado

--- src/viking.js
+++ src/viking.js
@@ -106,17 +106,38 @@ this.vikingArmy.push(viking);
 }
 
 vikingAttack() {
-  return ;
+  const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+    const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+    const damage = this.saxonArmy[randomSaxon].receiveDamage(
+      this.vikingArmy[randomViking].strength
+    );
+    if (this.saxonArmy[randomSaxon].health <= 0) {
+      this.saxonArmy.pop(randomSaxon);
+    }
+    return damage;
 }
 
 saxonAttack(){
-  this.health -= damage;
-  this.health;
+  const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+  const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+  const damage = this.vikingArmy[randomViking].receiveDamage(
+    this.saxonArmy[randomSaxon].strength
+  );
+  if (this.vikingArmy[randomViking].health <= 0) {
+    this.vikingArmy.pop(randomViking);
+  }
+  return damage;
   
 }
 
 showStatus() {
-
+  if (this.saxonArmy.length <= 0) {
+  return `Vikings have won the war of the century!`
+}else if (this.vikingArmy.length <= 0) {
+  return `Saxons have fought for their lives and survived another day...`
+}else {
+  return `Vikings and Saxons are still in the thick of battle.`
+}
 }
 
 }
